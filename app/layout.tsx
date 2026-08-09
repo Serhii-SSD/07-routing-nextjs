b500b8fcb1a9f25dd3c7a9374ca819db03211717
Serhii-SSD <demydenko.ss@gmail.com>
2026-08-09 12:51:01 +0300

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+import TanStackProvider from '../components/TanStackProvider/TanStackProvider';
+import 'modern-normalize/modern-normalize.css';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'NoteHub',
+  description: 'A simple and efficient application for managing personal notes',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="uk">
+      <body>
+        <TanStackProvider>
+          <Header />
+          {children}
+          <Footer />
+        </TanStackProvider>
+      </body>
+    </html>
+  );
+}
